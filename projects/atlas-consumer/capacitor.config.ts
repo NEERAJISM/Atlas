d999614389c82f6cd4696d21903acb322c361fcf
NEERAJISM <8patidarneeraj@gmail.com>
2021-11-22 02:53:32 +0530

    20211122 - ionic capacitor android integration : atlas-consumer

---
+++ projects/atlas-consumer/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.atlas.consumer',
+  appName: 'atlas-consumer',
+  webDir: '../../dist/atlas-consumer',
+  bundledWebRuntime: false
+};
+
+export default config;
